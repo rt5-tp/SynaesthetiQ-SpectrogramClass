078d9fd6d8382d7573874224843c5db0918c9f84
Tom Burnip <tb@tburnip.co.uk>
2023-04-12 13:53:47 +0100

    Generating and adding test data

--- test/TestSpectrogram.cpp
+++ test/TestSpectrogram.cpp
@@ -3,12 +3,15 @@
 
 
 TEST(runTwoSecondSpectrogram,successSpectrogram){
+
+    
+
     // Using a provided audio sample create two seconds worth of display (aprox 40 frames).
     Spectrogram spectrogram();
 
-    for (int sample; sample < 88200; sample += 2048){
+    // for (int sample; sample < 88200; sample += 2048){
         
-    }
+    // }
     spectrogram.bufferCallback():
     // 
 }
