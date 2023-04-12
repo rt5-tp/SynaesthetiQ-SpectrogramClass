be4cee88e9cd0681c1ec67797d26dd7431946f23
Tom Burnip <tb@tburnip.co.uk>
2023-04-12 11:13:56 +0100

    Spectrogram class

---
+++ spectrogram.hpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include "Colour.hpp"
+#include <arm_neon.h>
+#include "turboColourMap.hpp"
+
+
+class spectrogram
+{
+private:
+    /* data */
+    Colour greyToColour(uint8_t);
+public:
+    spectrogram(/* args */);
+    ~spectrogram();
+
+    // Ideal buffer length is hard to determine. 
+    // Use 128 tap points. 
+    // Every 11 buffers we create a frame giving us roughly 31 fps.
+    void bufferCallback(const std::vector<double> &);
+
+    std::vector<uint32_t> acumBuffer;
+    int bufferLength;
+    int bufferNumber;
+
+    std::vector<uint8_t> display; // 32*16 XOffset
+    uint8_t xOffset; 
+    
+};
+
+
+
+spectrogram::spectrogram(/* args */)
+{
+}
+
+spectrogram::~spectrogram()
+{
+}
+
+Colour spectrogram::greyToColour(uint8_t grey) {
+    return turboColourMap[grey];
+}
+
+void spectrogram::bufferCallback(const std::vector<double> &inputBuffer) {
+    // Every 11 buffers we need to render a frame.
+    // 129 Element Array. Accumulate each buffer into the Array. Divide array by 11.
+
+    std::vector<int> yAccum = [1,1,2,2,4,5,8,12,17,25,35,51,73,105,150,215];
+    
+    int offset = 0;
+    for (int i; i < 16; i++) {
+        int acum = 0;
+
+        for (int j; j < yAccum[i]; i++) {
+            acum += inputBuffer[j + offset];
+        }
+
+        int x = (i+xOffset) % 32;
+        display[i + x*32] = acum;
+        offset += yAccum[i];
+    }
+    // Ring Buffer for vertical lines.
+};
